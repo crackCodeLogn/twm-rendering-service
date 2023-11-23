43ce8a059c016e7ba58f56f5a6d151d3f67539a0
Vivek <vivektheintel@gmail.com>
2023-11-23 15:47:02 -0500

    Upgrading spring, swagger, shell script and app version

--- src/main/java/com/vv/personal/twm/render/RenderingServer.java
+++ src/main/java/com/vv/personal/twm/render/RenderingServer.java
@@ -6,19 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.time.ZoneId;
+import java.util.TimeZone;
 
 import static com.vv.personal.twm.render.constants.Constants.*;
 
@@ -26,7 +25,7 @@ import static com.vv.personal.twm.render.constants.Constants.*;
  * @author Vivek
  * @since 17/11/20
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @ComponentScan({"com.vv.personal.twm.render", "com.vv.personal.twm.ping"})
 @SpringBootApplication
@@ -37,20 +36,13 @@ public class RenderingServer {
     private Environment environment;
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("EST", ZoneId.SHORT_IDS))); //force setting
         SpringApplication.run(RenderingServer.class, args);
     }
 
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
         return new ProtobufHttpMessageConverter();
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.vv.personal.twm.render"))
-                .build();
     }
 
     @EventListener(ApplicationReadyEvent.class)
